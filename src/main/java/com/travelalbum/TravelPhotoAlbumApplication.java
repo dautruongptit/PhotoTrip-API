3f1266694de4a8531d394b3c0781a5c484cb8643
truongdq <truongdq@seatechit.com.vn>
2026-07-31 14:16:24 +0700

    commit dev

--- src/main/java/com/travelalbum/TravelPhotoAlbumApplication.java
+++ src/main/java/com/travelalbum/TravelPhotoAlbumApplication.java
@@ -12,17 +12,13 @@ public class TravelPhotoAlbumApplication {
 
     public static void main(String[] args) {
         // Đọc trực tiếp file .env.dev ở root dự án (song song với pom.xml)
-        try (InputStream input = new FileInputStream(".env.dev")) {
+        try (InputStream input = new FileInputStream(".env")) {
             Properties prop = new Properties();
             prop.load(input);
             prop.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
-            System.out.println(">>> Loaded .env.dev successfully from root!");
         } catch (Exception e) {
             System.err.println(">>> ERROR: Could not find .env.dev in root directory!");
         }
-
-        System.out.println(">>> JWT_SECRET = " + System.getProperty("JWT_SECRET"));
-
         SpringApplication.run(TravelPhotoAlbumApplication.class, args);
     }
 }
